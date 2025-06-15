1d9a2c984cd4d8095257a02a1b17ca8a43a2d9ba
Eros Secundus <156208632+erossecundus@users.noreply.github.com>
2025-06-15 16:38:43 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// script principal da aplicação
+
+// carrega filmes do localStorage ou mockados
+const filmesSalvos = JSON.parse(localStorage.getItem("filmes")) || filmesMockados;
+
+// Carregar e exibir cards
+const container = document.getElementById("movie-list");
+filmesSalvos.forEach(filme => {
+  container.innerHTML += createMovieCard(filme);
+});
+
+// filtrando pela busca
+document.getElementById("inputSearchMyMovies").addEventListener("input", function () {
+  const busca = this.value.toLowerCase();
+  const cards = document.querySelectorAll(".card-filme"); // ou o seletor usado
+
+  cards.forEach(card => {
+    const titulo = card.querySelector(".card-title").textContent.toLowerCase();
+    if (titulo.includes(busca)) {
+      card.classList.remove("d-none");
+    } else {
+      card.classList.add("d-none");
+    }
+  });
+});
